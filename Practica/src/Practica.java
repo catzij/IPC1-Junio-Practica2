6555eaf5fb77762f5c7af4ee0432ba25f7c32024
Josué Casiá <josuecasia201831432@cunoc.edu.gt>
2021-06-13 00:20:32 -0600

    Practica.java Muestra y llena Clientes

--- Practica/src/Practica.java
+++ Practica/src/Practica.java
@@ -7,8 +7,8 @@ public class Practica{
 	String[] nombreClientes = new String[100];
 	int[] idClientes = new int[100];
 	int[] telefonoClientes = new int[100];
-	int[] peliculaCliente = new int[100];
-	           	
+	int[] peliculaCliente = new int[100];	
+
 	public static void main(String args[]){
 
 		Practica nuevo = new Practica();
@@ -17,27 +17,29 @@ public class Practica{
 	public Practica(){
 
 		int idCliente = 0;
-		int opcion;
-		
-
-		Bienvenido(idCliente);
+		int seleeccionMenu;	
 
 		do {
+				asterisco(40);
+				Bienvenido();
                 menu();
                 System.out.print("Elija una opcion: ");
-                opcion = read.nextInt();
-                opcionMenu(opcion);
-            } while (opcion != 9);		
+                seleeccionMenu = read.nextInt();
+                seleccionMenu(seleeccionMenu,idCliente);
+                if (seleeccionMenu==6) {
+                	idCliente++;
+                }                
+            } while (seleeccionMenu != 9);
+        System.out.println("ADIOS");
 	}
 
-	public void Bienvenido(int i){
+	public void Bienvenido(){
 
-		System.out.println(" *** BIENVENIDO A MEMORABILIA *** ");
-		System.out.println(" Que encuentres la pelicual que buscas "+nombreClientes[i]);
+		System.out.println("    *** BIENVENIDO A MEMORABILIA *** ");
 	}
 
 	public void menu(){
-
+		asterisco(40);
 		System.out.println("      *** Menu *** ");
 		System.out.println("1. Prestamo de peliculas");
 		System.out.println("2. Devolucion de peliculas");
@@ -45,85 +47,112 @@ public class Practica{
 		System.out.println("4. Ingreso de peliculas");
 		System.out.println("5. Ordenar peliculas");
 		System.out.println("6. Ingresar clientes nuevos");
-		System.out.println("7. Mostrar Clietes");
-		System.out.println("8. Reportes");		
+		System.out.println("7. Mostrar Clientes");
+		System.out.println("8. Reportes");
 		System.out.println("9. SALIR");
 	}
 
-	public void opcionMenu(int opcion){
+	public void asterisco(int limite){
 
-        switch(opcion){
-            case 1:{           
-                borrar_pantalla();
-                System.out.println("Ingreso a la opcion 1");
-                break;
-            }
-            case 2:{
-                borrar_pantalla();                
-                System.out.println("Ingreso a la opcion 2");
-                break;
-            }
-            case 3:{
-            	borrar_pantalla();
-            	System.out.println("Ingreso a la opcion 3");
-                break;
-            }
-            case 4:{
-            	borrar_pantalla();
-            	System.out.println("Ingreso a la opcion 4");                            
-                break;
-            }
-            case 5:{
-            	borrar_pantalla();
-            	System.out.println("Ingreso a la opcion 5");
-                break;
-            }
-            case 6:{
-            	borrar_pantalla();
-            	System.out.println("Ingreso a la opcion 6");
-            	llenarCLiente(nombreClientes, idClientes, telefonoClientes);
-            	break;
-            }
-            case 7:{
-            	borrar_pantalla();
-            	System.out.println("Ingreso a la opcion 7");
-            	break;
-            }
-            case 8:{
-            	borrar_pantalla();
-            	System.out.println("Ingreso a la opcion 8");
-            	break;
-            }
-        }
-    }
-
-	public void clienteInicial(String[] nombres, int[] idCLiente, int[] telefono){
-
-		int clientecero =0;
-		nombres[clientecero] = "Eduardo";
-		idCLiente[clientecero] = clientecero;		
-		telefono[clientecero] = 12345678;		
+		for (int i = 0;  i<limite;i++ ) {
+			System.out.print("*");	
+		}
+		System.out.println("");
 	}
 
-	public void llenarCLiente(String[] nombres, int[] idCLiente, int[] telefono){
+	public int contar(int variable){
 
-		int contador=1;
+		int numero = 0;
+		for (int i = 0; i < (variable+1); i++ ) {
+			numero++;
+		}
+		return numero;
+	}
+
+	public void seleccionMenu(int seleccion, int id){			
+
+		switch(seleccion){
+            case 1:{
+                borrar_pantalla();
+                System.out.println("Ingreso a la opcion 1. Prestamo de peliculas");
+                pausa();
+                borrar_pantalla();
+                break;
+            }case 2:{
+                borrar_pantalla();
+                System.out.println("Ingreso a la opcion 2. Devolucion de peliculas");
+                pausa();
+                borrar_pantalla();
+                break;
+            }case 3:{
+            	borrar_pantalla();
+            	System.out.println("Ingreso a la opcion 3. Mostrar las peliculas");
+            	pausa();
+            	borrar_pantalla();
+                break;
+            }case 4:{
+            	borrar_pantalla();
+            	System.out.println("Ingreso a la opcion 4. Ingreso peliculas");
+            	pausa();
+            	borrar_pantalla();
+                break;
+            }case 5:{
+            	borrar_pantalla();
+            	System.out.println("Ingreso a la opcion 5. Ordenar Peliculas");
+            	pausa();
+            	borrar_pantalla();
+                break;
+            }case 6:{            	
+            	borrar_pantalla();
+            	System.out.println("Ingreso a la opcion 6. Ingresar Nuevos Clientes");
+            	llenarCLiente(nombreClientes, idClientes, telefonoClientes, id);                        
+            	pausa();
+            	borrar_pantalla();
+            	break;
+            }case 7:{
+
+            	borrar_pantalla();
+           	 	System.out.println("Ingreso a la opcion 7. Mostrar Clientes");            	
+            	mostrarClientes(nombreClientes, idClientes, telefonoClientes, id);
+            	pausa();
+            	borrar_pantalla();
+            	break;
+            }case 8:{
+            	borrar_pantalla();
+            	System.out.println("Ingreso a la opcion 8. Reportes");
+            	pausa();
+            	borrar_pantalla();
+            	break;
+            }
+
+        }
 		
+		
+    }
+
+	public void llenarCLiente(String[] nombres, int[] idCLiente, int[] telefono,int contador){
+		
+		idCLiente[contador] = contador;
 		System.out.println("Ingrese nombre : ");
-		nombres[contador] = read.nextLine();
-		idCLiente[contador] = contador;		
+		nombres[contador] = read.next();
 		System.out.println("Ingrese numero de telefono:");
-		telefono[contador] = read.nextInt();				
+		telefono[contador] = read.nextInt();					
+	}
+
+	public void mostrarClientes(String[] nombres, int[] idCLiente, int[] telefono, int cliente){
 		
-		contador++;
-		
-		System.out.println("\n\n");
-		//return contador;
+		if (cliente==0) {
+			System.out.println("no hay clientes ");
+		}else{
+			for (int i=0 ;i < cliente ;i++ ) {
+				System.out.println("ID Cliente "+idCLiente[i]+" Nombre "+nombres[i]+" Telefono " +telefono[i]);
+			}
+		}
 	}
 
     public static void pausa(){
         //Interrupcion para hacer pausa en la pantalla
-        new java.util.Scanner(System.in).nextLine(); 
+        new java.util.Scanner(System.in).nextLine();
     }
 
     public static void borrar_pantalla() {
@@ -132,3 +161,5 @@ public class Practica{
         System.out.flush();
     }
 }
+
+
